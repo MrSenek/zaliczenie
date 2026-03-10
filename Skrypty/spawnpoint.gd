1093013c2970ca78baa2b7abf6cefb9bde667425
MrSenek <30927883+MrSenek@users.noreply.github.com>
2026-03-10 14:08:35 +0100

    spawnpointy

--- Skrypty/spawnpoint.gd
+++ Skrypty/spawnpoint.gd
@@ -1,11 +1,19 @@
 extends Node2D
+@onready var enemy_scene : PackedScene = preload("res://Sceny/Przeciwnik.tscn")
+@export var player: Node2D	
+@onready var timer: Timer = $Timer
+
+func spawn_enemy():
+	var player_location = player.global_position
+	var distance_to_player = player_location.x - global_position.x
+	if distance_to_player > 200 and timer.is_stopped():
+		var create_enemy = enemy_scene.instantiate()
+		add_child(create_enemy)
+		timer.start(5)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	spawn_enemy()
