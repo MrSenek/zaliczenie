754155058682588215c259435b21d7e98e799e59
MrSenek <30927883+MrSenek@users.noreply.github.com>
2026-04-14 12:11:47 +0200

    fixing shop item titles

--- scenes/shop_map/items.gd
+++ scenes/shop_map/items.gd
@@ -1,23 +1,20 @@
 extends Node2D
 
-var items_list = ["+10HP", "Electric Weapon","Gravity Grenade", "Self Guiding Missile", "1,5x Damage"]
+var org_items_list = ["+10HP", "Electric Weapon","Gravity Grenade", "Self Guiding Missile", "1,5x Damage"]
 
+var items_list = org_items_list
 func _ready() -> void:
 	var item1 = items_list.pick_random()
-	get_node("Item1/Label").text = item1
+	get_node("Item1/Node2D/Label").text = item1
+	items_list.erase(item1)
 	
 	var item2 = items_list.pick_random()
-	while item1 == item2:
-		item2 = items_list.pick_random()
-	get_node("Item2/Label").text = item2
+	get_node("Item2/Node2D/Label").text = item2
+	items_list.erase(item2)
 	
 	var item3 = items_list.pick_random()
-	while item1 == item3 and item2 == item3:
-		item3 = items_list.pick_random()
-	get_node("Item3/Label").text = item3
-	
-	
-	
+	get_node("Item3/Node2D/Label").text = item3
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
