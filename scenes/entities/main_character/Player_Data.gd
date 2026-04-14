dbf0157362e29cce87a389bd86549f5e4853d0b2
MrSenek <mr.senek268@gmail.com>
2026-04-15 00:41:32 +0200

    Improve shop items, player stats & label scale
    Change PlayerStats.attack from int to float (default 1) to support fractional multipliers. Refactor shop items script: store picked items in module-level vars, update labels, erase chosen items, add give_item() to apply effects (HP, weapons, damage multiplier) and queue_free purchased nodes, and restrict body enter/exit handlers to the Player group. Also adjust Item Node2D scales in the shop scene from 0.15 to 0.1 for all three items.

--- scenes/entities/main_character/Player_Data.gd
+++ scenes/entities/main_character/Player_Data.gd
@@ -3,7 +3,7 @@ extends Resource
 class_name PlayerStats
 @export var max_health: int = 100
 @export var defence: int = 10
-@export var attack: int = 10
+@export var attack: float = 1
 var player_coins = 0
 var current_health = 10
 var owned_weapons = ["fireball"]
--- scenes/shop_map/items.gd
+++ scenes/shop_map/items.gd
@@ -11,45 +11,60 @@ var player_in = {
 @onready var item_2: Area2D = $Item2
 @onready var item_3: Area2D = $Item3
 
-
-
+var item1
+var item2
+var item3
 
 func _ready() -> void:
-	var item1 = items_list.pick_random()
+	item1 = items_list.pick_random()
 	get_node("Item1/Node2D/Label").text = item1
 	items_list.erase(item1)
 	
-	var item2 = items_list.pick_random()
+	item2 = items_list.pick_random()
 	get_node("Item2/Node2D/Label").text = item2
 	items_list.erase(item2)
 	
-	var item3 = items_list.pick_random()
+	item3 = items_list.pick_random()
 	get_node("Item3/Node2D/Label").text = item3
 
 func _process(delta: float) -> void:
 	if player_in["item1"] == true and Input.is_action_just_pressed("strzal"):
 		item_2.queue_free()
 		item_3.queue_free()
+		item_1.queue_free()
+		give_item(item1)
+
+
+func give_item(item):
+	if item == "+10HP":
+		PlayerData.max_health+=10
+	if item == "Electric Weapon":
+		PlayerData.owned_weapons.append("Electric Weapon")
+	if item == "Gravity Grenade":
+		PlayerData.owned_weapons.append("Gravity Grenade")
+	if item == "Self Guiding Missile":
+		PlayerData.owned_weapons.append("Self Guiding Missile")
+	if item == "1,5x Damage":
+		PlayerData.attack = 1.5
+
+
+
 
 func _on_item_1_body_entered(body: Node2D) -> void:
-	player_in["item1"] = true
-
-
+	if body.is_in_group("Player"):
+		player_in["item1"] = true
 func _on_item_2_body_entered(body: Node2D) -> void:
-	player_in["item2"] = true
-
-
+	if body.is_in_group("Player"):
+		player_in["item2"] = true
 func _on_item_3_body_entered(body: Node2D) -> void:
-	player_in["item3"] = true
-
-
+	if body.is_in_group("Player"):
+		player_in["item3"] = true
 func _on_item_1_body_exited(body: Node2D) -> void:
-	player_in["item1"] = false
-
-
+	if body.is_in_group("Player"):
+		player_in["item1"] = false
 func _on_item_2_body_exited(body: Node2D) -> void:
-	player_in["item2"] = false
-
-
+	if body.is_in_group("Player"):
+		player_in["item2"] = false
 func _on_item_3_body_exited(body: Node2D) -> void:
-	player_in["item3"] = false
+	if body.is_in_group("Player"):
+		player_in["item3"] = false
